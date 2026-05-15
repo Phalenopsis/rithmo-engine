118e28da68cdc6765d995ce8d897d2c51c2f14f3
Nicolas Leroux <hertford.fr@gmail.com>
2026-05-15 10:10:34 +0200

    feat(Reintroduction): compute options where a player can reintroduce a captured piece

---
+++ src/main/java/eu/nicosworld/rithmo/engine/reintroduction/Reintroduction.java
@@ -0,0 +1,7 @@
+package eu.nicosworld.rithmo.engine.reintroduction;
+
+import eu.nicosworld.rithmo.engine.model.Piece;
+import eu.nicosworld.rithmo.engine.model.Position;
+
+public record Reintroduction(Piece piece, Position position) {
+}
