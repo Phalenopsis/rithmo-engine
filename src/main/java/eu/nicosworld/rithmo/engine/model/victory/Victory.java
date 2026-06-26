4a2166a62912edb026845f47106246a16557bbfb
Nicolas Leroux <hertford.fr@gmail.com>
2026-06-26 18:15:59 +0200

    feat(victory) : add ProperVictory

--- src/main/java/eu/nicosworld/rithmo/engine/model/victory/Victory.java
+++ src/main/java/eu/nicosworld/rithmo/engine/model/victory/Victory.java
@@ -2,6 +2,6 @@ package eu.nicosworld.rithmo.engine.model.victory;
 
 import eu.nicosworld.rithmo.engine.model.Player;
 
-public sealed interface Victory permits BodyVictory, GoodsVictory, LawsuitVictory {
+public sealed interface Victory permits BodyVictory, GoodsVictory, LawsuitVictory, ProperVictory {
   Player winner();
 }
